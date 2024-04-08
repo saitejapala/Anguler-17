ea0e2210410b87186426d8633d890a01a933bb80
Sai Teja <saitejapala+github@gmail.com>
2024-04-08 20:04:36 +0530

    file created with one child

---
+++ ComponentCommunication3/src/app/child1/child1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child1',
+  standalone: true,
+  imports: [],
+  templateUrl: './child1.component.html',
+  styleUrl: './child1.component.css'
+})
+export class Child1Component {
+
+}
